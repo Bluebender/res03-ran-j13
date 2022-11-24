38ae4588357cccb153b61e5fe90885e78f24594b
Bluebender <olliviev@gmail.com>
2022-11-24 14:01:16 +0000

    Troisième sauvegarde

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@ window.addEventListener("DOMContentLoaded", function(event){
 
 });
 
-let body = document.querySelector("body");
-
 let red = document.getElementById("red");
 let orange = document.getElementById("orange");
 let yellow = document.getElementById("yellow");
@@ -18,54 +16,83 @@ let positionGreen = green.getBoundingClientRect().top;
 let positionBlue = blue.getBoundingClientRect().top;
 let positionPurple = purple.getBoundingClientRect().top;
 
-console.log(positionRed, positionOrange, 
-positionYellow, positionGreen, positionBlue, positionPurple);
+// function getNextSection(current){
+//     if (current.id==="red"){
+//         return document.getElementById("orange");
+//     }
+//     if (current.id==="orange"){
+//         return document.getElementById("yellow");
+//     }
+//     if (current.id==="yellow"){
+//         return document.getElementById("green");
+//     }
+//     if (current.id==="green"){
+//         return document.getElementById("blue");
+//     }
+//     if (current.id==="blue"){
+//         return document.getElementById("purple");
+//     }
+// }
 
-let currentScrollPosition = document.documentElement.scrollTop;
-console.log(currentScrollPosition);
-
-function getNextSection(current){
-    if (current.id==="red"){
-        return document.getElementById("orange");
-    }
-    if (current.id==="orange"){
-        return document.getElementById("yellow");
-    }
-    if (current.id==="yellow"){
-        return document.getElementById("green");
-    }
-    if (current.id==="green"){
-        return document.getElementById("blue");
-    }
-    if (current.id==="blue"){
-        return document.getElementById("purple");
-    }
-    
-    
-    
-}
-
-
-
-
-
-
+let compteurScroll = 0
 
 document.addEventListener("wheel", function(event){
+    if(compteurScroll>=3){
+        compteurScroll=0
+    }
+    compteurScroll = compteurScroll +1;
     
     if(event.deltaY > 0) // je vais vers le bas
     {
-        let currentScrollPosition = document.documentElement.scrollTop;
-        console.log(positionRed);
         
-        if (currentScrollPosition===positionRed)
-        // let positionToGo = positionOrange;
-        scroll(currentScrollPosition, positionGreen);
+        
+        console.log(compteurScroll);
+        if (compteurScroll===3){       
+
+            let currentScrollPosition = document.documentElement.scrollTop;
+
+            if (currentScrollPosition===positionRed){
+                scroll(0, positionOrange);
+                
+            }
+            if (currentScrollPosition===positionOrange){
+                scroll(0, positionYellow);
+            }
+            if (currentScrollPosition===positionYellow){
+                scroll(0, positionGreen);
+            }
+            if (currentScrollPosition===positionGreen){
+                scroll(0, positionBlue);
+            }
+            if (currentScrollPosition===positionBlue){
+                scroll(0, positionPurple);
+            }
+        }
     }
+
     else if(event.deltaY < 0) // je vais vers le haut
     {
-        let positionToGo = -400;
-        scroll(0, positionToGo);
+        console.log(compteurScroll);
+        if (compteurScroll===3){  
+            
+            let currentScrollPosition = document.documentElement.scrollTop;
+
+            if (currentScrollPosition===positionPurple){
+                scroll(0, positionBlue);
+            }
+            if (currentScrollPosition===positionBlue){
+                scroll(0, positionGreen);
+            }
+            if (currentScrollPosition===positionGreen){
+                scroll(0, positionYellow);
+            }
+            if (currentScrollPosition===positionYellow){
+                scroll(0, positionOrange);
+            }
+            if (currentScrollPosition===positionOrange){
+                scroll(0, positionRed);
+            }
+        }
     }
     
 })
